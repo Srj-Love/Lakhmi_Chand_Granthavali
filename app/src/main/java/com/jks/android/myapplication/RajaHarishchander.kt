e8aecb058e2331ada2ad0e403e56bc7e594979c4
Srj-Love <gupta.suraj2102@gmail.com>
2019-12-05 15:39:06 +0530

    Added Firebase

---
+++ app/src/main/java/com/jks/android/myapplication/RajaHarishchander.kt
@@ -0,0 +1,306 @@
+package com.jks.android.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RajaHarishchander : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_raja_harishchander)
+        val button1 = findViewById<View>(R.id.rhk) as Button
+        val button2 = findViewById<View>(R.id.rh1) as Button
+        val button3 = findViewById<View>(R.id.rh2) as Button
+        val button4 = findViewById<View>(R.id.rh3) as Button
+        val button5 = findViewById<View>(R.id.rh4) as Button
+        val button6 = findViewById<View>(R.id.rh5) as Button
+        val button7 = findViewById<View>(R.id.rh6) as Button
+        val button8 = findViewById<View>(R.id.rh7) as Button
+        val button9 = findViewById<View>(R.id.rh8) as Button
+        val button10 = findViewById<View>(R.id.rh9) as Button
+        val button11 = findViewById<View>(R.id.rh10) as Button
+        val button12 = findViewById<View>(R.id.rh11) as Button
+        val button13 = findViewById<View>(R.id.rh12) as Button
+        val button14 = findViewById<View>(R.id.rh13) as Button
+        val button15 = findViewById<View>(R.id.rh14) as Button
+        val button16 = findViewById<View>(R.id.rh15) as Button
+        val button17 = findViewById<View>(R.id.rh16) as Button
+        val button18 = findViewById<View>(R.id.rh17) as Button
+        val button19 = findViewById<View>(R.id.rh18) as Button
+        val button20 = findViewById<View>(R.id.rh19) as Button
+        val button21 = findViewById<View>(R.id.rh20) as Button
+        val button22 = findViewById<View>(R.id.rh21) as Button
+        val button23 = findViewById<View>(R.id.rh22) as Button
+        val button24 = findViewById<View>(R.id.rh23) as Button
+        val button25 = findViewById<View>(R.id.rh24) as Button
+        val button26 = findViewById<View>(R.id.rh25) as Button
+        val button27 = findViewById<View>(R.id.rh26) as Button
+        val button28 = findViewById<View>(R.id.rh27) as Button
+        val button29 = findViewById<View>(R.id.rh28) as Button
+        val button30 = findViewById<View>(R.id.rh29) as Button
+        val button31 = findViewById<View>(R.id.rh30) as Button
+        val button32 = findViewById<View>(R.id.rh31) as Button
+        val button33 = findViewById<View>(R.id.rh32) as Button
+        val button34 = findViewById<View>(R.id.rh33) as Button
+        val button35 = findViewById<View>(R.id.rh34) as Button
+        val button36 = findViewById<View>(R.id.rh35) as Button
+        val button37 = findViewById<View>(R.id.rh36) as Button
+        val button38 = findViewById<View>(R.id.rh37) as Button
+        val button39 = findViewById<View>(R.id.rh38) as Button
+        val button40 = findViewById<View>(R.id.rh39) as Button
+        val button41 = findViewById<View>(R.id.rh40) as Button
+        val button42 = findViewById<View>(R.id.rh41) as Button
+        val button43 = findViewById<View>(R.id.rh42) as Button
+        val button44 = findViewById<View>(R.id.rh43) as Button
+        val button45 = findViewById<View>(R.id.rh44) as Button
+        val button46 = findViewById<View>(R.id.rh45) as Button
+        val button47 = findViewById<View>(R.id.rh46) as Button
+        val button48 = findViewById<View>(R.id.rh47) as Button
+        val button49 = findViewById<View>(R.id.rh48) as Button
+        val button50 = findViewById<View>(R.id.rh49) as Button
+        val button51 = findViewById<View>(R.id.rh50) as Button
+        val button52 = findViewById<View>(R.id.rh51) as Button
+        val button53 = findViewById<View>(R.id.rh52) as Button
+        val button54 = findViewById<View>(R.id.rh53) as Button
+        val button55 = findViewById<View>(R.id.rh54) as Button
+        val button56 = findViewById<View>(R.id.rh55) as Button
+        val button57 = findViewById<View>(R.id.rh56) as Button
+        val button58 = findViewById<View>(R.id.rh57) as Button
+
+
+        button1.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rhk::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh1::class.java)
+            startActivity(intent)
+        }
+        button3.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh2::class.java)
+            startActivity(intent)
+        }
+        button4.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh3::class.java)
+            startActivity(intent)
+        }
+        button5.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh4::class.java)
+            startActivity(intent)
+        }
+        button6.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh5::class.java)
+            startActivity(intent)
+        }
+        button7.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh6::class.java)
+            startActivity(intent)
+        }
+        button8.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh7::class.java)
+            startActivity(intent)
+        }
+        button9.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh8::class.java)
+            startActivity(intent)
+        }
+        button10.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh9::class.java)
+            startActivity(intent)
+        }
+        button11.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh10::class.java)
+            startActivity(intent)
+        }
+        button12.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh11::class.java)
+            startActivity(intent)
+        }
+        button13.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh12::class.java)
+            startActivity(intent)
+        }
+        button14.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh13::class.java)
+            startActivity(intent)
+        }
+        button15.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh14::class.java)
+            startActivity(intent)
+        }
+        button16.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh15::class.java)
+            startActivity(intent)
+        }
+        button17.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh16::class.java)
+            startActivity(intent)
+        }
+        button18.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh17::class.java)
+            startActivity(intent)
+        }
+        button19.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh18::class.java)
+            startActivity(intent)
+        }
+        button20.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh19::class.java)
+            startActivity(intent)
+        }
+        button21.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh20::class.java)
+            startActivity(intent)
+        }
+        button22.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh21::class.java)
+            startActivity(intent)
+        }
+        button23.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh22::class.java)
+            startActivity(intent)
+        }
+        button24.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh23::class.java)
+            startActivity(intent)
+        }
+        button25.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh24::class.java)
+            startActivity(intent)
+        }
+        button26.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh25::class.java)
+            startActivity(intent)
+        }
+        button27.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh26::class.java)
+            startActivity(intent)
+        }
+        button28.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh27::class.java)
+            startActivity(intent)
+        }
+        button29.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh28::class.java)
+            startActivity(intent)
+        }
+        button30.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh29::class.java)
+            startActivity(intent)
+        }
+        button31.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh30::class.java)
+            startActivity(intent)
+        }
+        button32.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh31::class.java)
+            startActivity(intent)
+        }
+        button33.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh32::class.java)
+            startActivity(intent)
+        }
+        button34.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh33::class.java)
+            startActivity(intent)
+        }
+        button35.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh34::class.java)
+            startActivity(intent)
+        }
+        button36.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh35::class.java)
+            startActivity(intent)
+        }
+        button37.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh36::class.java)
+            startActivity(intent)
+        }
+        button38.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh37::class.java)
+            startActivity(intent)
+        }
+        button39.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh38::class.java)
+            startActivity(intent)
+        }
+        button40.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh39::class.java)
+            startActivity(intent)
+        }
+        button41.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh40::class.java)
+            startActivity(intent)
+        }
+        button42.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh41::class.java)
+            startActivity(intent)
+        }
+        button43.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh42::class.java)
+            startActivity(intent)
+        }
+        button44.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh43::class.java)
+            startActivity(intent)
+        }
+        button45.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh44::class.java)
+            startActivity(intent)
+        }
+        button46.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh45::class.java)
+            startActivity(intent)
+        }
+        button47.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh46::class.java)
+            startActivity(intent)
+        }
+        button48.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh47::class.java)
+            startActivity(intent)
+        }
+        button49.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh48::class.java)
+            startActivity(intent)
+        }
+        button50.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh49::class.java)
+            startActivity(intent)
+        }
+        button51.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh50::class.java)
+            startActivity(intent)
+        }
+        button52.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh51::class.java)
+            startActivity(intent)
+        }
+        button53.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh52::class.java)
+            startActivity(intent)
+        }
+        button54.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh53::class.java)
+            startActivity(intent)
+        }
+        button55.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh54::class.java)
+            startActivity(intent)
+        }
+        button56.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh55::class.java)
+            startActivity(intent)
+        }
+        button57.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh56::class.java)
+            startActivity(intent)
+        }
+        button58.setOnClickListener {
+            val intent = Intent(this@RajaHarishchander, rh57::class.java)
+            startActivity(intent)
+        }
+    }
+}
