9ce0c2c902aacef221c6b4f2dd6ac70af31c5b57
Srj-Love <gupta.suraj2102@gmail.com>
2019-12-09 03:43:24 +0530

    Added Database Constant Field

---
+++ app/src/main/java/com/jks/android/myapplication/utils/DBConstant.kt
@@ -0,0 +1,24 @@
+package com.jks.android.myapplication.utils
+
+import android.provider.BaseColumns
+import com.jks.android.myapplication.BuildConfig
+
+class DBConstant {
+
+
+    companion object{
+
+        const val _ID = BaseColumns._ID
+        const val ID = "id"
+        const val NAME = "name"
+
+        const val SUB_ID = "sub_id"
+        const val SUB_NAME = "subname"
+
+        const val STORY = "story"
+
+        const val EXTRA = "extra"
+
+    }
+
+}
